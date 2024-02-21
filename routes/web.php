eb963be4ae90f6827900366c3d7d542baac98e3b
nizarmahi <mahinizar7@gmail.com>
2024-02-21 10:32:07 +0700

    Perubahan Route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});   
+Route::get('/world', function () {
+    return 'World';
 });
+
+// Praktikum  1
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function() {
+    return 'NIM : 2241720185 <br> Nama : Mochammad Nizar Mahi';
+});
+
+// Praktikum 2 Route dengan Parameter
+// Route::get('/user/{name}', function ($name) {
+//     return 'Nama saya '.$name;
+// });
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman artikel dengan id '. $id;
+});
+
+// Praktikum 3 Route  Optional Parameter
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
